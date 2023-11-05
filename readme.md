ef2f8b92719ae27002a0457531ee32b77b853a2c
Maytrok <maytrok@gmx.de>
2023-11-05 11:47:10 +0100

    First commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+simple php script to update dyndns at selfhost.eu
+
+Remember to copy the .env.example to .env and fill your credentials
---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace src;
+
+include "../vendor/autoload.php";
+
+use Dotenv\Dotenv;
+use GuzzleHttp\Client;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . "\\..\\");
+$dotenv->load();
+
+$url = "https://carol.selfhost.de/update?username=%s&password=%s";
+
+$client = new Client();
+$res = $client->request('GET', sprintf($url, $_ENV['username'], $_ENV['password']));
+echo $res->getBody();
